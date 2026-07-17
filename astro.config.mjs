768d1ee2ef39127bd0be6bfe7710afa53dfe3ba3
Matt Allen <matt_allen@utexas.edu>
2026-07-17 07:15:13 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  // Replace with your actual domain once connected.
-  // Used for canonical URLs and sitemaps.
   site: 'https://stargazerworkshop.com',
 });
